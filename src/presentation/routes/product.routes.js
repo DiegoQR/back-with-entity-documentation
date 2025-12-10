650e7d5811acf8ef2817346eee9b1132f656cf5a
DiegoQR <diegoqr2000@gmail.com>
2025-12-10 19:27:47 -0400

    Products endpoints documented in swagger

--- src/presentation/routes/product.routes.js
+++ src/presentation/routes/product.routes.js
@@ -14,10 +14,140 @@ const productService = new ProductService(productRepository);
 const productController = new ProductController(productService);
 
 const router = Router();
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     tags:
+ *       - Products
+ *     summary: Retrieve a list of the products in the store
+ *     responses:
+ *       200:
+ *         description: A list of products.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 router.get('/', asyncHandler(productController.getAll));
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     tags:
+ *       - Products
+ *     summary: Retrieve a single product by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A single product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: Product not found
+ */
 router.get('/:id', asyncHandler(productController.getById));
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     tags:
+ *       - Products
+ *     summary: Create a new product
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ProductInput'
+ *     responses:
+ *       201:
+ *         description: The created product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: No token provided, authorization denied
+ *       403:
+ *         description: Admin privileges required
+ *     security:
+ *       - bearerAuth: []
+ *
+ */
 router.post('/', [authenticateToken, isAdmin], asyncHandler(productController.create));
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     tags:
+ *       - Products
+ *     summary: Update a product
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ProductInput'
+ *     responses:
+ *       200:
+ *         description: The updated product.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ProductInput'
+ *       404:
+ *         description: Product not found
+ *       401:
+ *         description: No token provided, authorization denied
+ *       403:
+ *         description: Admin privileges required
+ *     security:
+ *       - bearerAuth: []
+ */
 router.put('/:id', [authenticateToken, isAdmin], asyncHandler(productController.update));
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     tags:
+ *       - Products
+ *     summary: Delete a product
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: No content
+ *       404:
+ *         description: User not found
+ *       401:
+ *         description: No token provided, authorization denied
+ *       403:
+ *         description: Admin privileges required
+ *     security:
+ *       - bearerAuth: []
+ */
 router.delete('/:id', [authenticateToken, isAdmin], asyncHandler(productController.delete));
 
 module.exports = router;
